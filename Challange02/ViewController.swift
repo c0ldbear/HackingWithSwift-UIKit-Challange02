c5f785fdf99793f3519af04985d9594a9e6b78e3
teddy <teddyjh@protonmail.com>
2022-09-01 08:39:53 +0200

    Add items and display in the table

--- Challange02/ViewController.swift
+++ Challange02/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class TableViewController: UITableViewController {
 
+    var items = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if items.isEmpty {
+            items = ["potato", "apple", "milk", "heavy cream", "tofu"]
+        }
+        
+        tableView.reloadData()
     }
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Item", for: indexPath)
+        var cellContent = cell.defaultContentConfiguration()
+        cellContent.text = items[indexPath.row].capitalized
+        cell.contentConfiguration = cellContent
+        return cell
+    }
 
 }
 
