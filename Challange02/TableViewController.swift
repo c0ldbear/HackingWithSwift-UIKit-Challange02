6bbccbcff7f2d762420fa5b6bef745016102141f
teddy <teddyjh@protonmail.com>
2022-09-01 08:50:23 +0200

    Add + button and alert to add items to list

--- Challange02/TableViewController.swift
+++ Challange02/TableViewController.swift
@@ -14,6 +14,10 @@ class TableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "🛒 Shopping List"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAddItem))
+        
         if items.isEmpty {
             items = ["potato", "apple", "milk", "heavy cream", "tofu"]
         }
@@ -31,6 +35,24 @@ class TableViewController: UITableViewController {
         cellContent.text = items[indexPath.row].capitalized
         cell.contentConfiguration = cellContent
         return cell
+    }
+    
+    @objc func promptForAddItem() {
+        let ac = UIAlertController(title: "Add item", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [weak self, weak ac] action in // we declare them as being weak so that Swift won’t create a strong reference cycle.
+            guard let answer = ac?.textFields?[0].text else { return }
+            self?.submit(answer)
+        }
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func submit(_ answer: String) {
+        let indexPath = IndexPath(row: 0, section: 0)
+        items.insert(answer.capitalized, at: 0)
+        tableView.insertRows(at: [indexPath], with: .automatic)
     }
 
 }
